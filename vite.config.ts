44ca0ad370498b970d8878e656d801dfd6207639
Rafał Majewski <goodheropl@gmail.com>
2025-04-16 09:18:44 +0000

    Deploying

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,8 @@
 import {sveltekit as createSvelteKitPlugins} from "@sveltejs/kit/vite";
 import type {UserConfig} from "vite";
 const svelteKitPlugins = await createSvelteKitPlugins();
-export default {plugins: svelteKitPlugins} as const satisfies UserConfig;
+export default {
+	plugins: svelteKitPlugins,
+	build: {outDir: "../build", emptyOutDir: true},
+	base: process.env["GITHUB_ACTIONS"] === "true" ? "/image-flow/" : "/",
+} as const satisfies UserConfig;
